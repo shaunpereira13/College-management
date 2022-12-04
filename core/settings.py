678164501608c8dddf5754b18ad52f506bb470e8
Atharva Parkhe <patharv777@gmail.com>
2022-12-04 19:16:11 +0530

    added a few tables

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage.apps.HomepageConfig'
+    'homepage.apps.HomepageConfig',
+    "student"
 ]
 
 MIDDLEWARE = [
@@ -53,12 +58,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+TEMPLATES_DIRS = [
+            os.path.join(BASE_DIR,'templates'),
+        ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR,'templates'),
-        ],
+        'DIRS': TEMPLATES_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,26 +84,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'my_database',  
-        'USER': 'root',  
-        'PASSWORD': 'start123',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
-}  
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'my_database',  
+#         'USER': 'root',  
+#         'PASSWORD': 'password',  
+#         'HOST': '127.0.0.1',  
+#         'PORT': '3306',  
+#         'OPTIONS': {  
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+#         }  
+#     }  
+# }  
 
 
 # Password validation
--- student/models.py
+++ student/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class StudentModel(models.Model):
+    roll_no = models.PositiveSmallIntegerField(unique=True)
+    pr_no = models.PositiveSmallIntegerField(unique=True)
+    name = models.CharField(max_length=50)
+    attendance = models.FloatField(default=0)
+    def __str__(self):
+        return self.name
+
+
+
+class SubjectModel(models.Model):
+    subject_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.subject_name
+
+
+class SubjectTakenModel(models.Model):
+    student = models.ForeignKey(StudentModel, on_delete=models.CASCADE)
+    subject = models.ForeignKey(SubjectModel, on_delete=models.CASCADE)
